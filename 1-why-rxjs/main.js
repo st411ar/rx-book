4f5d28c8ec40fc36aead26e47e95c09394b69f56
Vitaly Kiselev <st411ar@ya.ru>
2018-07-27 13:21:10 +0300

    refactored and added comments about migration from rxjs 4 to 5 in the chapter 1

--- 1-why-rxjs/main.js
+++ 1-why-rxjs/main.js
@@ -5,20 +5,25 @@ console.log('"main.js" start');
 var inputField = $('#input');
 var resultsField = $('#results');
 
+/* Only get the value from each key up */
 var keyups = Rx.Observable.fromEvent(inputField, 'keyup')
-		.map(e => e.target.value)
-		.filter(text => text.length > 2);
+        .map(e => e.target.value)
+        .filter(text => text.length > 2);
 
+/* Now throttle/debounce the input for 500ms */
+// migration 4 -> 5 : throttle -> throttleTime
 var throttled = keyups.throttleTime(500);
 
+/* Now get only distinct values,
+so we eliminate the arrows and other control characters */
 var distinct = throttled.distinctUntilChanged();
 
 var usedObservable = distinct;
 
 usedObservable.subscribe(
-	(text) => {
-		console.log('keyups event with result value: "' + text + '"');
-	}
+    (text) => {
+        console.log('keyups event with result value: "' + text + '"');
+    }
 );
 
 function searchWikipedia (term) {
@@ -33,20 +38,27 @@ function searchWikipedia (term) {
     }).promise();
 }
 
+// migration 4 -> 5 : flatMapLatest -> switchMap
 var suggestions = usedObservable.switchMap(searchWikipedia);
 
-suggestions.subscribe(data => {
-    var res = data[1];
+suggestions.subscribe(
+    data => {
+        var res = data[1];
 
-    /* Do something with the data like binding */
-    resultsField.empty();
+        /* Do something with the data like binding */
+        resultsField.empty();
 
-    $.each(res, (_, value) => $('<li>' + value + '</li>').appendTo(resultsField));
-}, error => {
-    /* handle any errors */
-    resultsField.empty();
+        $.each(
+            res,
+            (_, value) => $('<li>' + value + '</li>').appendTo(resultsField)
+        );
+    },
+    error => {
+        /* handle any errors */
+        resultsField.empty();
 
-    $('<li>Error: ' + error + '</li>').appendTo(resultsField);
-});
+        $('<li>Error: ' + error + '</li>').appendTo(resultsField);
+    }
+);
 
 console.log('"main.js" stop');
